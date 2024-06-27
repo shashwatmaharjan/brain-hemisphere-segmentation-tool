40a7c7c37c6d91b18a979d0a30cda325f95873f7
shashwatmaharjan <mahar1s@cmich.edu>
2024-06-26 22:33:37 -0400

    Created a pop-up to select a folder

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,24 @@
 # Import necessary libraries
 import os
 import platform
+import tkinter as tk
+
+from tkinter import filedialog
+
+# Define necessary functions
+# Function to select a folder
+def select_folder(initial_directory):
+    
+    # Define a root window
+    root = tk.Tk()
+
+    # Hide the root window
+    root.withdraw()
+
+    # Select a folder
+    folder_selected = filedialog.askdirectory(initialdir=initial_directory, title='Sample You Want to Annotate')
+
+    return folder_selected
 
 # Main function
 def main():
@@ -9,6 +27,9 @@ def main():
     current_directory = os.getcwd()
     data_directory = os.path.join(current_directory, 'data')
 
+    # Start a pop-up window to select a folder
+    selected_folder = select_folder(initial_directory=data_directory)
+    
 if __name__ == '__main__':
 
     # Check the platform
